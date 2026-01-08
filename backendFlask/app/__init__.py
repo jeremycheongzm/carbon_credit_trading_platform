9577241e594dd9a4d985760e0747fc04b4f0a7b7
jeremycheongzm <115978571+jeremycheongzm@users.noreply.github.com>
2026-01-08 08:21:43 +0000

    update backendFlask

--- backendFlask/app/__init__.py
+++ backendFlask/app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 from .config import Config
-from .routes.auth_routes import auth_bp
-from .routes.request_routes import request_bp
+# from .routes.auth_routes import auth_bp
+# from .routes.request_routes import request_bp
+from .extensions import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
     CORS(app)  # Enable Cross-Origin Resource Sharing
+
+    from .routes.auth_routes import auth_bp
+    from .routes.request_routes import request_bp
 
     # Register blueprints (modularize routes)
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
