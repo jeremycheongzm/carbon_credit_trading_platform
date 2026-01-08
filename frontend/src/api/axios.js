d1270db283dd3825eb6650f6727828c9d1647a87
jeremycheongzm <115978571+jeremycheongzm@users.noreply.github.com>
2026-01-08 22:39:02 +0800

    update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,21 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
 
-export default instance;
+export default api;
+
